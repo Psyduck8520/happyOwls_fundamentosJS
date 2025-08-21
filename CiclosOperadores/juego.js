d32b70018b0234b321ae1fe830c6b6268496b241
Edison Quimbiamba <exprogresive@gmail.com>
2025-08-21 17:36:28 -0500

    Juego condicionales

---
+++ CiclosOperadores/juego.js
@@ -0,0 +1,13 @@
+const numeroSecreto =  Math.floor(Math.random()  * 10   + 1);
+const numeroJugador =  parseInt( prompt("Adivina el numero de 1 a 10"))
+
+console.log(`Este es el numero con el que juegas ${numeroJugador}`)
+
+if ( numeroJugador ===  numeroSecreto){
+    console.log("Felicidades, adivinaste el numero  secreto")
+
+}else if(numeroJugador < numeroSecreto) {
+    console.log("El numero es demasiado. bajo, intenta de nuevo") 
+} else  {
+    console.log("El numero es muy alto intente de nuevo")
+}
