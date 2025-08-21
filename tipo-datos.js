f814f76105f459b6021996336182cdf78fecd864
Edison Quimbiamba <exprogresive@gmail.com>
2025-08-21 11:58:04 -0500

    feat: tipos de datos

---
+++ tipo-datos.js
@@ -0,0 +1,40 @@
+//PRIMITITVOS
+
+//STRING
+let nombre =  'edi'
+
+// Number
+let   edad =  25
+
+//boolean
+let esMayorDeEdad = true 
+
+// null
+let  noHayValor = null
+
+ //undefined
+ let noDefinido = undefined // no los da js  pero podemos utilizarlo 
+
+ //symbol
+let simboloUnico =  Symbol('unico') //aqui tenemos un valor completamete unico
+  
+//biginit
+ let numeroGrande  = 2n 
+
+//Complejos
+//object
+let  carro = {
+    marca: 'tesla', 
+    modelo: 'Model'
+}
+//Array
+let frutas = [
+     'manzanas', 'Pina' ,  'banana'
+] 
+ 
+//Functions , las funciones tambien son un tipo de dato  en js. EXISTEN muchas maneras de escribirlas
+//
+function saludar () {
+      
+}
+
