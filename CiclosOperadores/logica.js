31a6381b080723451c8c9b73247d4c1020c7025c
Edison Quimbiamba <exprogresive@gmail.com>
2025-08-21 16:51:36 -0500

    Operadores logicos

---
+++ CiclosOperadores/logica.js
@@ -0,0 +1,15 @@
+/*
+ Operadadore logicos
+&&   si los dos se cumplen
+||. o s uno de los dos es verdad
+!  si este valor no es esto 
+*/
+ 
+const a = 10;
+const b = 20;
+const c = 10;
+console.log(  a ===b  &&  a === c) //false por que una de las dos es falsa
+console.log( a != b || a ===c) // true por que una de estas dos se cumple
+console.log( !(a === c )) //Se gace una validacion
+
+
