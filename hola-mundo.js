c6c72e67c55b39c46ff1ca469dd6d4d6614d0487
Edison Quimbiamba <exprogresive@gmail.com>
2025-08-21 11:17:02 -0500

    feat: primeros fundamentos de JS para proyecto educativo

---
+++ hola-mundo.js
@@ -0,0 +1,23 @@
+console.log('hola  mundo feliz')  
+let   cajaDeAndy  =  'woody'
+console.log(cajaDeAndy) //Paar el nombramiento esto tiene que se bastante descriptivo, siempre. tenem
+// LO no permitido
+
+let c = 'woody'
+let cda = 'woody'
+let pcAndy = 'WOODY'
+
+// Lo que si esta permitido, si utilizamo varias. variables tienen que ir al inicio de nuestro  arhivo , se puede utilizar ;
+
+let primerTransteoDeAndy  =  'Woody'
+let urlDelUsuario = 'https://www.google.com'
+let idDelUsuario  =   '122323' 
+
+// Cuando usamos  let podemos decir que esta variable  va tomar varios valores como lo vemos a continuacion
+let contador  = 0
+contador = contador + 1 
+console.log(contador)
+
+ //valores contantes ofijas  que es una constante
+const pi  = 3.14 
+console.log('hola mundo')
