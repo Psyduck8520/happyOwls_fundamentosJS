058c2a04c2059a68e62f946b80663c44788ed251
Edison Quimbiamba <exprogresive@gmail.com>
2025-08-21 16:44:23 -0500

    Operadores de comparacion

---
+++ CiclosOperadores/index.js
@@ -0,0 +1,24 @@
+//Operadores de comparacion
+
+/* 
+==  si algo es igual a otra cosa 
+=== en el valor el tipo. de dato 
+!=  diferente. de  nos va dar una negacion
+ !== diferente en el valor el tipo de dato 
+> 
+<
+>=
+<= 
+
+*/
+
+const a = 10 
+const b =  20
+const c = "10" 
+
+console.log( a==b)
+
+console.log( a===b)
+
+
+
