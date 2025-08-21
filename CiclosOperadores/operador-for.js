2dd107699269ea2392b162c056dedaf4d6d422e3
Edison Quimbiamba <exprogresive@gmail.com>
2025-08-21 18:29:56 -0500

    Operador for

---
+++ CiclosOperadores/operador-for.js
@@ -0,0 +1,9 @@
+/*
+for ( variable; codicion ;  incremento ){
+    codigo que se va ejecutar;
+}*/
+
+let  list  = [ 'eat',  'sleep' , 'code',  'repeat'] ;
+for ( let i = 0 ; i< list.length ;  i ++ ) {
+     console.log(list[i])
+}
